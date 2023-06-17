b3324bbcfe8968cfcec49bc0103614bf2aef385d
Usuario <Usuario@DESKTOP-2PIQBH7>
2023-06-17 10:37:24 -0300

    Adicionar arquivos de projeto.

---
+++ Startup.cs
@@ -0,0 +1,37 @@
+﻿namespace	BarDaGraça
+
+public class Startup
+{
+	public Startup(IConfiguration configuration)
+	{
+		Configuration = configuration;
+	}
+	public IConfiguration Configuration { get;}
+
+	public void ConfigureServices(IServiceColection services)
+	{
+        Services.AddControllersWithViews();
+        services.AddMvc();
+    }
+	
+	public void Configure(WebApplication app, IWebHostEnviroment enviroment)
+	{
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+    }
+}
